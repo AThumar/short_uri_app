01c7c4fd196487aee0798ab476acf394421f704e
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-02-06 09:41:00 +0530

    generating random and remove duplication

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const url = require("url");
 const fs = require("fs");
 const app = express();
-const port = 3000;
+const port = 3001;
 app.get("/readfile", (req, res) => {
   //   var data = fs.readFileSync("data.json");
   //   res.send(data);
@@ -11,16 +11,28 @@ app.get("/readfile", (req, res) => {
     else res.send(data);
   });
 });
+function readData() {
+  var data = fs.readFileSync("data.json");
+  data = JSON.parse(data);
+  return data;
+}
+function saveData(data) {
+  fs.writeFileSync("data.json", JSON.stringify(data));
+}
 app.get("/", (req, res) => {
   console.log(req.query.url);
   var parsedUrl = url.parse(req.query.url);
-  console.log("domain", parsedUrl.hostname);
-  console.log("host", parsedUrl.host);
-  console.log("path", parsedUrl.path);
-  console.log("port", parsedUrl.port);
-  console.log("protocol", parsedUrl.protocol);
-  console.log("parsedUrl", parsedUrl);
-
+  if (parsedUrl.protocol && parsedUrl.hostname) {
+    var shortcode = Math.floor(Math.random() * 100000);
+    var data = readData();
+    if (
+      !data[shortcode] &&
+      !Object.keys(data).find((k) => data[k] == parsedUrl.ref)
+    )
+      data[shortcode] = parsedUrl.href;
+    data[shortcode] = parsedUrl.href;
+    saveData(data);
+  }
   res.send("Hello World!");
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
